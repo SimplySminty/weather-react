6981a2ff8212f23eb85ed41de18cbeb57beeb7de
Sarah Minton <sarahminton2000@gmail.com>
2025-05-23 11:21:03 -0400

    added footer info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="Weather-App">Weather App</div>;
+  return (
+    <div className="Weather-App">
+      <h1>Weather App</h1>
+      <footer>
+        This app was coded by Sarah Minton and is open-sourced on{" "}
+        <a href="https://github.com/SimplySminty/weather-react" target="_blank">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
