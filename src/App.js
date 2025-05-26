21d033d9db7c3f61bf401d9e89c7f02294c30d6b
Sarah Minton <sarahminton2000@gmail.com>
2025-05-26 14:37:32 -0400

    added bootstap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
+import "bootstrap/dist/css/boostrap.css";
 
 export default function App() {
   return (
     <div className="Weather-App">
-      <h1>Weather App</h1>
-      <footer>
-        This app was coded by Sarah Minton and is open-sourced on{" "}
-        <a href="https://github.com/SimplySminty/weather-react" target="_blank">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This app was coded by Sarah Minton and is open-sourced on{" "}
+          <a
+            href="https://github.com/SimplySminty/weather-react"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
